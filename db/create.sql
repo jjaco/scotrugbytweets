fd94f4251cc1194737dee51b675363fa3008ffa2
Jason Costello <jjacostello@gmail.com>
2018-12-28 14:59:03 +0000

    Move from timestamp ms to parsed DateTime object

--- db/create.sql
+++ db/create.sql
@@ -1,7 +1,7 @@
 \c scotrugbytweet;
 CREATE TABLE tweets (
  id VARCHAR PRIMARY KEY,
- timestamp BIGINT NOT NULL,
+ timestamp TIMESTAMP NOT NULL,
  tweet VARCHAR NOT NULL,
  entities VARCHAR NOT NULL
 );
