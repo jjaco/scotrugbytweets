e791a73db39b9e25902e27d48495df9591b75381
Jason Costello <jjacostello@gmail.com>
2019-01-15 01:11:49 +0000

    Change of NLP library to Spacy

--- listener/nlp.py
+++ listener/nlp.py
@@ -1,24 +1,26 @@
 from nltk import word_tokenize, pos_tag, ne_chunk
 from nltk.tree import Tree
 
+import spacy
+
+nlp = spacy.load('en')
+
 def clean_tweet(tweet):
-	twitter_punc = ['#', '@']
-	terms_to_remove = ['Edinburgh', 'Glasgow', 'Scotstoun', 
+    twitter_punc = ['#', '@']
+    terms_to_remove = ['Edinburgh', 'Glasgow', 'Scotstoun', 
         'Murrayfield', 'Scotland', 'Warrior',
         'England', 'Wales', 'Italy', 'France',
         'Ireland', 'Pro14', 'Guinness']
-	filter_list = twitter_punc + terms_to_remove
+    filter_list = twitter_punc + terms_to_remove
 
-	filtered = [word for word in tweet.split() if not any(punc in word for punc in filter_list)]
+    filtered = [word for word in tweet.split() if not any(punc in word for punc in filter_list)]
 
-	return ' '.join(filtered)
+    return ' '.join(filtered)
 
 
 def extract_entities(tweet):
-	cleaned_tweet = clean_tweet(tweet)
-	part_of_speech = pos_tag(word_tokenize(cleaned_tweet))
-	chunked_pos = ne_chunk(part_of_speech)
-	tree_entities = [chunk for chunk in chunked_pos if type(chunk) == Tree]
-	flat_entities = [[name for (name, label) in entity] for entity in tree_entities]
+    cleaned_tweet = clean_tweet(tweet)
+    parsed = nlp(cleaned_tweet).ents
+    entities = list([entity.text for entity in parsed if entity.label_ == 'PERSON'])
 
-	return str([" ".join(name) for name in flat_entities])
+    return entities
