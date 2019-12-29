05305b0d836e0da5ddc7ed78e3c5a655e683e895
Jason Costello <jjacostello@gmail.com>
2019-12-29 23:03:21 +0000

    Port visualisation over to Dash (plotly tool)

--- frontend/app.py
+++ frontend/app.py
@@ -3,61 +3,53 @@ from sqlalchemy import create_engine
 import pandas as pd
 from itertools import chain
 
-from bokeh.embed import components
-from bokeh.plotting import figure
-from bokeh.resources import INLINE
-from bokeh.util.string import encode_utf8
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+import plotly.graph_objects as graph_obj
 
-import io
-import base64
 
-app = Flask(__name__)
+def create_app():
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+    return app
+
+
+app = create_app()
+
+app.layout = html.Div([
+    html.H2(children='jocka'),
+    html.Button("Query", id="query_data"),
+    html.Div(id="graph")
+])
 
 engine = create_engine('postgresql://postgres:postgres@db:5432/scotrugbytweet')
 
-df_query = (
-	pd.read_sql("""
-		SELECT entities 
-		FROM tweets 
+df_query = pd.read_sql_query("""
+		SELECT entities
+		FROM tweets
 		WHERE timestamp > NOW() - INTERVAL '24 hours'
 		AND LENGTH(entities) > 3
 		""", engine)
-	)
 df_query['entities'] = df_query['entities'].apply(lambda row: row[1:-1].split(', '))
 entities = pd.Series([row[1:-1] for row in list(chain(*df_query['entities'].values))])
 
-@app.route('/dash')
-def bokeh():
 
+@app.callback(Output('graph', 'children'), [Input('query_data', component_property='n_clicks')])
+def graph(n):
     N = 10
     top_N_entities = entities.value_counts()[:N]
+
+    print(top_N_entities)
 
     x = list(top_N_entities.index)
     y = list(top_N_entities.values)
 
-    fig = figure(plot_width=600, plot_height=600, x_range=x)
-    fig.vbar(
-        x=x,
-        top=y,
-        color='navy',
-        width=0.9
-    )
-
-    # grab the static resources
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
-
-    # render template
-    script, div = components(fig)
-    html = render_template(
-        'index.html',
-        plot_script=script,
-        plot_div=div,
-        js_resources=js_resources,
-        css_resources=css_resources,
-    )
-    return encode_utf8(html)
+    bar = graph_obj.Bar(x=x, y=y, name='Results')
+    return html.Div([dcc.Graph(id="bar_plot", figure=graph_obj.Figure(data=bar))])
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run_server(host='0.0.0.0', debug=True)
